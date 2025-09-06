ba6cc50ea86f477587c55d29ee8bcd03419e3d23
eford <eford@users.noreply.github.com>
2025-09-06 00:02:49 +0000

    Deploying to gh-pages from @ PsuAstro528/lab2@6658fd51604642e566294a0b9b7de314d66b1521 🚀

---
+++ test/test1.jl
@@ -0,0 +1,63 @@
+using Test
+
+@testset "Testing solution to Exercise 1" begin
+
+@testset "Running ex1.jl" begin
+   include("../ex1.jl")
+end;
+
+@testset "Testing that variables exist" begin
+   #@test @isdefined(response_1a)
+   #@test @isdefined(response_1b)
+   @test @isdefined(response_1c)
+   @test @isdefined(response_1d)
+   @test @isdefined(response_1e)
+   @test @isdefined(response_1f)
+   @test @isdefined(response_1g)
+   @test @isdefined(response_1h)
+   @test @isdefined(response_1i)
+   @test @isdefined(response_1j)
+   @test @isdefined(response_1k)
+   @test @isdefined(response_1l)
+   #@test @isdefined(response_1m)
+   @test @isdefined(response_1n)
+   @test @isdefined(response_1o)
+   @test @isdefined(response_1p)
+end;
+
+@testset "Testing that variables are not missing" begin
+   #@test !ismissing(response_1a)
+   #@test !ismissing(response_1b)
+   @test !ismissing(response_1c)
+   @test !ismissing(response_1d)
+   @test !ismissing(response_1e)
+   @test !ismissing(response_1f)
+   @test !ismissing(response_1g)
+   @test !ismissing(response_1h)
+   @test !ismissing(response_1i)
+   @test !ismissing(response_1j)
+   @test !ismissing(response_1k)
+   @test !ismissing(response_1l)
+   #@test !ismissing(response_1m)
+   @test !ismissing(response_1n)
+   @test !ismissing(response_1o)
+   @test !ismissing(response_1p)
+end;
+
+@testset "Testing numerical answers" begin
+   maxN_8gb = floor(Int,sqrt(8*2^30/8))
+	@test response_1c == maxN_8gb
+	maxN_1tb = floor(Int,sqrt(1024*2^30/8))
+   @test response_1f == maxN_1tb
+        my_cpu_speed =  has_cpu_frequencies() ? cpu_base_frequency()*1e6 : 1e9
+        my_flops = 1 * my_cpu_speed  # Ball-park assumption, depends on CPU architecture
+	my_est_1d = (2//3)*maxN_8gb^3 / my_flops
+	@test my_est_1d/10 <= response_1d <= 10*my_est_1d
+	my_est_1g = (2//3)*maxN_1tb^3 / my_flops
+	@test my_est_1g/10 <= response_1g <= 10*my_est_1g
+	my_est_1i = (2//3)*100^3 / my_flops
+	@test my_est_1i/10 <= response_1i <= 10*my_est_1i
+end;
+
+
+end; # Exercise 1
